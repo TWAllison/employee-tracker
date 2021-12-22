05f8ea9937add0284397aace7f4e98aa8d6762a1
TWAllison <tylerallison55@gmail.com>
2021-12-22 07:52:26 -0700

    schema/ create tables for department, roles, and employee

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,32 @@
 DROP DATABASE if exists employee_tracker;
 CREATE DATABASE employee_tracker;
 USE employee_tracker; 
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_dept
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT,
+    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_role
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+);
