4b7ba30f8ec2246baca223153e18fabd8ce73912
TWAllison <tylerallison55@gmail.com>
2021-12-22 08:22:27 -0700

    set required packages and set connection to server with err handling, psudo code

---
+++ index.js
@@ -0,0 +1,15 @@
+// add required packages
+
+// create server connection port 3306
+
+//add err handling if err if not call search function
+
+//create db search function 
+
+// create function to view all employees 
+
+// create function to view all departments 
+
+//create function to view all roles
+
+// add more functionality ? add emloyee, roles etc maybe update ? delete function?
