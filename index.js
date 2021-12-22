07e8c112d03d9cc2896bc83b453eb07a50355050
TWAllison <tylerallison55@gmail.com>
2021-12-22 09:42:33 -0700

    add functions to view roles, employees, and departments

--- index.js
+++ index.js
@@ -57,8 +57,40 @@ function searchDb() {
 
 // create function to view all employees
 
+function viewAllEmployees() {
+  const query =
+    "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id AS title, employee.manager_id, roles.title, roles.salary, department.deptartment_name AS dept FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id";
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+
+    searchDb();
+  });
+}
 // create function to view all departments
 
+function viewAllDepartments() {
+  const query = "SELECT * FROM department";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+
+    searchDb();
+  });
+}
+
 //create function to view all roles
+
+function viewRoles() {
+  const query =
+    "SELECT roles.id, roles.title, roles.salary, roles.department_id, department.id, department.deptartment_name FROM roles LEFT JOIN department on roles.department_id = department.id";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    //prompt user to make a selection again
+    searchDb();
+  });
+}
 
 // add more functionality ? add emloyee, roles etc maybe update ? delete function?
